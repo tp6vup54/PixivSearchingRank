58d95c1242b060b1dce6d88d15ab9146de1ab8c9
tp6vup54 <genius091612@gmail.com>
2016-10-03 03:04:17 +0800

    Try to add border.

---
+++ website/templates/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.thumbnail').addEventListener('mouseover', function(event) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+}, false);
